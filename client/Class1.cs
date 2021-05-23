7c04aa0fd8a328578e4ffef9660778ddf674159e
Shahzaib Hassan <shahzaib.hassan@ghostsoftware.local>
2021-05-23 19:36:00 +0500

    Add project files.

---
+++ client/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPSocket s = new UDPSocket();
+            s.Server("127.0.0.1", 27000); 
+            Console.WriteLine("================================");
+            Console.WriteLine("=   Connected to the server    =");
+            Console.WriteLine("================================");
+            Console.WriteLine("Waiting for response...");
+
+
+            UDPSocket c = new UDPSocket();
+            c.Client("127.0.0.1", 27000);
+            c.Send("TEST for udp from client !");
+
+            Console.ReadKey();
+        }
+    }
+}
